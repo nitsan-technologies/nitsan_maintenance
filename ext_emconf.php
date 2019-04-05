aaef3cc33d5f60136fe10a523dd45bfa6a3263d0
NITSAN Technologies <sanjay@nitsan.in>
2019-04-05 20:59:13 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,20 +14,18 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => '[NITSAN] Maintenance Mode',
   'description' => 'Do you want to make your site temporary offline like Coming soon, Maintenance Mode? This extension will help you with simple plug & play which have special TYPO3 Page & Template. It would be useful on criteria like to set Site Coming Soon Page, Maintanance of Site, Offline while Upgrading your site etc.,',
   'category' => 'plugin',
-  'author' => 'Sanjay Chuahan',
+  'author' => 'NITSAN Technologies',
   'author_email' => 'sanjay@nitsan.in',
   'state' => 'beta',
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.2.0',
+  'version' => '1.3.0',
   'constraints' => 
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-8.9.9',
-      'extbase' => '6.2.0-8.9.9',
-      'fluid' => '6.2.0-8.9.9',
+      'typo3' => '6.2.0-8.9.9'
     ),
     'conflicts' => 
     array (
