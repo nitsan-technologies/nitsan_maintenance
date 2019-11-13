1b76c1ea79fe59400253cfb4e47b37fb618696ee
NITSAN <sanjay@nitsan.in>
2019-11-13 11:57:56 +0530

    [TODO] Documentation Update!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nitsan_maintenance
-The TYPO3 Extension: https://extensions.typo3.org/extension/nitsan_maintenance/
+- FREE version: You can download FREE version with basic-features at https://extensions.typo3.org/extension/nitsan_maintenance/
+- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/nitsan-maintenance-pro/
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => '[NITSAN] Maintenance Mode',
-    'description' => 'Do you want to make your site temporary offline like Coming soon, Maintenance Mode? This extension will help you with simple plug & play which have special TYPO3 Page & Template. It would be useful on criteria like to set Site Coming Soon Page, Maintanance of Site, Offline while Upgrading your site etc.,',
+    'description' => 'Do you want to make your site temporary offline like coming soon and maintenance mode? This extension will help you with simple plug & play to your TYPO3 instance. You can download PRO version with more-features & free-support at https://t3terminal.com/nitsan-maintenance-pro',
     'category' => 'plugin',
     'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
 	'author_email' => 'sanjay@nitsan.in',
