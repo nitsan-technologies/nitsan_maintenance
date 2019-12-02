34e949a4f614c33925000342f68972f32933ee07
NITSAN <sanjay@nitsan.in>
2019-12-02 18:14:59 +0530

    [TODO] Adding Gitlab-CI & Travis with PHP, TypoScript, YAML lints [TODO] Updating documentation [TODO] Added demo link [TODO] Releasing pro-version of the extenion at https://t3terminal.com/

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => '[NITSAN] Maintenance Mode',
-    'description' => 'Do you want to make your site temporary offline like coming soon and maintenance mode? This extension will help you with simple plug & play to your TYPO3 instance. You can download PRO version with more-features & free-support at https://t3terminal.com/nitsan-maintenance-pro',
+    'description' => 'Do you want to make your site temporary offline like coming soon and maintenance mode? This extension will help you with simple plug & play to your TYPO3 instance. Demo: https://demo.t3terminal.com/t3t-extensions/maintenance-mode/ You can download PRO version for more-features & free-support at https://t3terminal.com/nitsan-maintenance-pro/',
     'category' => 'plugin',
     'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
 	'author_email' => 'sanjay@nitsan.in',
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '4.0.1',
+    'version' => '4.0.2',
     'constraints' => array(
         'depends' => array(
             'typo3' => '6.2.0-9.5.99',
