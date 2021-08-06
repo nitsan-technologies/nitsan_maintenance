e6db3b3d78d4aca71cb74a292b47000f2dda25cd
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2021-08-06 11:52:51 +0530

    [TODO] Code improvements

--- ext_tables.php
+++ ext_tables.php
@@ -24,7 +24,7 @@ if (TYPO3_MODE === 'BE') {
 }
 
 //# Add page TSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:nitsan_maintenance/Configuration/TypoScript/TsConfig/pageTsConfig.ts"/>');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:nitsan_maintenance/Configuration/TypoScript/TsConfig/pageTsConfig.typoscript"/>');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_nitsanmaintenance_domain_model_maintenance', 'EXT:nitsan_maintenance/Resources/Private/Language/locallang_csh_tx_nitsanmaintenance_domain_model_maintenance.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_nitsanmaintenance_domain_model_maintenance');
